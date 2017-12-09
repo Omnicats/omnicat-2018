3ddf26db6ca8389693f522467cd3afbb1f739433
student <windward_student@school.windwardschool.org>
2017-12-09 13:27:57 -0800

    Adds basicMotor class
    Adds basicMotor class with initial methods

---
+++ src/util/motor/basic/basicMotor.java
@@ -0,0 +1,92 @@
+package util.motor.basic;
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.*;
+
+public class basicMotor {
+	
+	public enum motorType{
+		CANTALON, TALON
+	}
+	
+	protected double speed;
+	public void setSpeed(double speed) {
+		switch(type) {
+			case CANTALON:
+				((CANTalon)controller).set(speed);
+				break;
+				
+			case TALON:
+				((CANTalon)controller).set(speed);
+				break;
+		}
+		this.speed = speed;
+	}
+	public double getSpeed() {
+		return speed;
+	}
+
+	protected Object controller;
+	public void setController(Object controller) {
+		this.controller = controller;
+	}
+	public Object getController() {
+		return controller;
+	}
+	
+	protected motorType type;
+	public void setType(motorType type) {
+		this.type = type;
+	}
+	public motorType getType() {
+		return type;
+	}
+	
+	protected boolean inverted;
+	public void setInverted(boolean inverted) {
+		this.inverted = inverted;
+		switch(type) {
+			case CANTALON:
+				((CANTalon)controller).setInverted(inverted);
+				break;
+				
+			case TALON:
+				((CANTalon)controller).setInverted(inverted);
+				break;
+		}	
+	}
+	public boolean getInverted() {
+		return inverted;
+	}
+	
+	public basicMotor(motorType type, int port) {
+		this(type, port, false);
+	}
+	
+	public basicMotor(motorType type, int port, boolean inverted) {
+		switch(type) {
+			case CANTALON:
+				setController(new CANTalon(port));
+				break;
+				
+			case TALON:
+				setController(new Talon(port));
+				break;
+		}
+		setType(type);
+		setInverted(inverted);
+	}
+
+		
+	public void rampTo(double target, double increment) {
+		if(Math.abs(getSpeed()-target) <= increment) {
+			setSpeed(target);
+		}
+		else if(getSpeed()<target) {
+			setSpeed(getSpeed()+increment);
+		}
+		else {
+			setSpeed(getSpeed()-increment);
+		}
+	}
+}
---
+++ src/util/motor/drive/drive.java
@@ -0,0 +1,12 @@
+package util.motor.drive;
+import edu.wpi.first.wpilibj.*;
+
+public class drive {
+	public enum driveType{
+		CANTALON, TALON
+	}
+
+	public drive() {
+		
+	}
+}
